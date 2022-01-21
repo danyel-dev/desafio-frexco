e3fe2c5333ac43c05ce408b78335df907a93025c
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-01-21 13:47:05 -0300

    mudando a chamada da api para o componente app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import axios from 'axios'
 
 import Header from './components/Header'
 import Home from './pages/Home';
 
 
 export default function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+      const fetchProducts = async () => {
+      const { data } = await axios.get('/api/fruit/all');
+      setProducts(data);
+      } 
+
+      fetchProducts()
+  }, [])
+
   return (
     <BrowserRouter>
       <Header />
@@ -13,7 +26,7 @@ export default function App() {
         path="/"
         exact
         render={() => (
-          <Home />
+          <Home products={ products } />
         )}
       />
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,10 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios'
-
 import { Container } from "@mui/material";
 
 import Title from "../components/Title";
 import ListProducts from '../components/ListProducts';
 
 
-export default function Home() {
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-        const { data } = await axios.get('/api/fruit/all');
-        setProducts(data);
-        } 
-
-        fetchProducts()
-    }, [])
-
+export default function Home({ products }) {
     return (
         <Container>
             <Title>Listagem das frutas</Title>
